0ef1a7b9f8a361b25e41ecd09616fe114c89a96e
Louise Serenhov <louiseserenhov@Louises-MacBook-Air.local>
2018-08-07 15:21:37 +0200

    IEfix

--- js/sleep.js
+++ js/sleep.js
@@ -37,6 +37,51 @@ $(document).ready(function () {
 
 //JAVASCRIPT KODEN 
 
+//Internet Explorer input field fix
+var nativePicker = document.querySelector('.nativeTimePicker');
+var fallbackPicker = document.querySelector('.fallbackTimePicker');
+
+var hourSelect = document.querySelector('#hour');
+var minuteSelect = document.querySelector('#minute');
+
+// hide fallback initially
+fallbackPicker.style.display = 'none';
+fallbackLabel.style.display = 'none';
+
+// test whether a new date input falls back to a text input or not
+var test = document.createElement('input');
+test.type = 'time';
+// if it does, run the code inside the if() {} block
+if(test.type === 'text') {
+  // hide the native picker and show the fallback
+  nativePicker.style.display = 'none';
+  fallbackPicker.style.display = 'block';
+
+  // populate the hours and minutes dynamically
+  populateHours();
+  populateMinutes();
+}
+
+function populateHours() {
+  // populate the hours <select> with the 6 open hours of the day
+  for(var i = 0; i <= 24; i++) {
+    var option = document.createElement('option');
+    option.textContent = i;
+    hourSelect.appendChild(option);
+  }
+}
+
+function populateMinutes() {
+  // populate the minutes <select> with the 60 hours of each minute
+  for(var i = 0; i <= 59; i++) {
+    var option = document.createElement('option');
+    option.textContent = (i < 10) ? ("0" + i) : i;
+    minuteSelect.appendChild(option);
+  }
+}
+
+
+
 function calculateNight(nightNr) {
 	console.log('kallar på nya funktionen calculateTotalBEdTimeForNigth' + nightNr);
 	var bedTime = document.getElementById('bedTimeDay' + nightNr).value;
